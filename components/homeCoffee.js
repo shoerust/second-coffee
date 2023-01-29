a33dec993ed55cb88b6ba4339465acbbef576108
William Southers <william@southers.id.au>
2023-01-29 13:33:36 +1100

    animate home

--- components/homeCoffee.js
+++ components/homeCoffee.js
@@ -5,7 +5,7 @@ import React, { useRef, useState, useEffect } from 'react'
 const HomeCoffee = props => {
 
     const canvasRef = useRef(null)
-    const [frameNumber, setFrameNumber] = useState(0);
+    const [frameNumber, setFrameNumber] = useState(32);
 
     const frameCount = 32;
     const frameCountWidth = 16;
@@ -14,27 +14,6 @@ const HomeCoffee = props => {
     const spriteWidth = 257;
     const spriteHeight = 257;
     const spriteSheet = "/images/sprites.png";
-
-    useEffect(() => {
-        const handleScroll = event => {
-
-            const scrollTop = window.scrollY;
-            const maxScrollTop = event.target.scrollingElement.scrollHeight - window.innerHeight;
-            const scrollFraction = scrollTop / maxScrollTop;
-            const frame = Math.ceil((scrollFraction * frameCount));
-            const frameNumber = Math.min(
-                frameCount - 1,
-                frame
-            );
-            setFrameNumber(frameNumber);
-        };
-
-        window.addEventListener('scroll', handleScroll);
-
-        return () => {
-            window.removeEventListener('scroll', handleScroll);
-        };
-    }, []);
 
     const draw = (context, img) => {
         var y = 0;
@@ -54,6 +33,11 @@ const HomeCoffee = props => {
             spriteWidth / scaleFactor,
             spriteHeight / scaleFactor
         );
+        window.requestAnimationFrame(() => {
+            if (frameNumber > 0) {
+                setFrameNumber(frameNumber - 1);
+            }
+        });
     }
 
     useEffect(() => {
