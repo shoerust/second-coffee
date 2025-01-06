56d14bae328da1d4d192ba34a18d3eb8390f2a2b
William Southers <william@southers.id.au>
2025-01-07 07:55:51 +1100

    feat(main): add poetry section and To Roam (#7)

---
+++ pages/poetry/[id].js
@@ -0,0 +1,45 @@
+import utilStyles from '../../styles/utils.module.css';
+import Layout from '../../components/layout';
+import Head from 'next/head';
+import Date from '../../components/date';
+import { getAllPostIds, getPostData } from '../../lib/posts';
+import path from 'path';
+
+const postsDirectory = path.join(process.cwd(), 'poetry');
+
+export async function getStaticProps({ params }) {
+    const postData = await getPostData(postsDirectory, params.id);
+    return {
+        props: {
+            postData,
+        },
+    };
+}
+
+export async function getStaticPaths() {
+    const paths = getAllPostIds(postsDirectory);
+    return {
+        paths,
+        fallback: false,
+    };
+}
+
+export default function PoetryPost({ postData }) {
+    return (
+      <Layout>
+        <Head>
+          <title>{postData.title}</title>
+        </Head>
+        <article>
+          <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+          {/*<div className={utilStyles.lightText}>*/}
+          {/*  <h4>by <a href={`/about`}>{postData.author}</a></h4>*/}
+          {/*</div>*/}
+          <div className={utilStyles.lightText}>
+            <Date dateString={postData.date} />
+          </div>
+          <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+        </article>
+      </Layout>
+    );
+  }
